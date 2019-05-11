3f103d1498189eb50ebc81de56b5c59193ffa08d
shijue1 <shengqiputixin@aliyun.com>
2019-05-11 15:20:47 +0800

    commit

--- pages/create-survey/index.js
+++ pages/create-survey/index.js
@@ -6,6 +6,7 @@ Page({
       showClass: 'none',
       data: {}
     },
+    moveableViewList: [],
     tapOne: {},
     pageInfo: {
       rowHeight: 0,
@@ -18,31 +19,68 @@ Page({
       selectedIndex: null,
     }
   },
-  addSurvey(e) {
-    this.setData({
-      surveyList: this.data.surveyList.push({
-        type: e.currentTarget.dataset.type,
-      }) && this.data.surveyList
-    })
+  touchEnd(e) {
+    debugger
   },
-  fetchHeight(index) {
-    var query = wx.createSelectorQuery();
-    //		//选择id
-    query.select('.aaaaabc').boundingClientRect()
-    query.exec(res =>{
-      this.data.pageInfo.rowHeight = res[index].height
-      // 初始化拖动控件数据
-      var movableViewInfo = this.data.movableViewInfo
-      movableViewInfo.data = this.data.surveyList[index]
-      movableViewInfo.showClass = "inline"
+  onMove(e) {
+    debugger
+  },
+  startMove(e) {
+    debugger
+  },
+  addSurvey(e) {
+    this.data.surveyList.push({
+      type: e.currentTarget.dataset.type,
+    })
+    this.data.moveableViewList.push({})
+    const index = this.data.surveyList.length - 1
+    //this.data.surveyList[index].height = 1
+    this.setData({
+      surveyList: this.data.surveyList
+    })
+    this.fetchHeight(index, '.aaa')
+      .then(el => {
+        Object.assign(this.data.moveableViewList[index], {
+          height: el.height * 2 + 6, // 解决 moveableView 设置 height ，实际渲染的高度会减少的问题，所以加上 6
+          y: this.calHeight(this.data.moveableViewList, 20),
+            
+        })
+        this.setData({
+          moveableViewList: this.data.moveableViewList
+        })
+      })
 
-      this.setData({
-        movableViewInfo: movableViewInfo,
-        pageInfo: this.data.pageInfo,
-        'movableViewInfo.y': this.data.pageInfo.startY - (this.data.pageInfo.rowHeight / 2)
+   
+  },
+  calHeight(array, offsetY) {
+    return array.reduce((acc, item, index, array) => {
+      return acc += index === 0
+              ? 0
+              : array[index - 1].height + offsetY
+    }, 0)
+  },
+  fetchHeight(index, el) {
+    return new Promise(resolve => {
+      var query = wx.createSelectorQuery();
+      //		//选择id
+      query.selectAll(el).boundingClientRect()
+      query.exec(res => {
+        resolve(res[0][index])
       })
     })
+    
+    // debugger
+    // this.data.pageInfo.rowHeight = res[index].height
+    // // 初始化拖动控件数据
+    // var movableViewInfo = this.data.movableViewInfo
+    // movableViewInfo.data = this.data.surveyList[index]
+    // movableViewInfo.showClass = "inline"
 
+    // this.setData({
+    //   movableViewInfo: movableViewInfo,
+    //   pageInfo: this.data.pageInfo,
+    //   'movableViewInfo.y': this.data.pageInfo.startY - (this.data.pageInfo.rowHeight / 2)
+    // })
   },
   dragStart: function (event) {
     var startIndex = event.target.dataset.index
@@ -130,6 +168,5 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
   },
 })
